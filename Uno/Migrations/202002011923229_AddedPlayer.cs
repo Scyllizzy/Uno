54c7fb33b0acf8fb96fd0ced595eab740de9124b
Robert Shields <roberthshields93@yahoo.com>
2020-02-01 11:26:19 -0800

    Created PlayerDb with GetAllPlayers(), Add(), Delete(), Update() methods.

---
+++ Uno/Migrations/202002011923229_AddedPlayer.cs
@@ -0,0 +1,33 @@
+﻿namespace Uno.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedPlayer : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Players",
+                c => new
+                    {
+                        PlayerID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(),
+                        Win = c.Int(nullable: false),
+                        Loss = c.Int(nullable: false),
+                        WinLossRatio = c.Double(nullable: false),
+                        UserName = c.String(),
+                        Password = c.String(),
+                    })
+                .PrimaryKey(t => t.PlayerID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Players");
+        }
+    }
+}
