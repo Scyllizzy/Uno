f5673ce4648a9df94bec941e8e2b65637991ee45
Makayla Reese <makaylareese15@gmail.com>
2020-03-15 20:31:35 -0700

    Added draw method

---
+++ Uno/Play Cards/Draw.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.Play_Cards
+{
+    class PlayCards
+    {
+        /// <summary>
+        /// Draw cards from 1 to 4.
+        /// </summary>
+        /// <param name="count">The draw cards draw amount or default 1 to just draw</param>
+        /// <param name="drawPile">The pile that has stored the cards that have not been used.</param>
+        public List<Card> Draw(int count, List<Card> drawPile)
+        {
+            List<Card> cards = new List<Card>();
+            for(int i = 0; i < count; i++)
+            {
+                //Remains zero because if i remove element zero te next element becomes zero.
+                cards.Add(drawPile[0]);
+                drawPile.RemoveAt(0);
+            }
+
+            return cards;
+        }
+
+    }
+}
